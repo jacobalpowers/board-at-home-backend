45e6c6e45f4e8b480de5f0d574f2e533b1384404
Jake Powers <jpowers1324@gmail.com>
2024-06-03 11:11:02 -0400

    More initial setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3001, () => {
+    console.log("I'm Listening");
+})
